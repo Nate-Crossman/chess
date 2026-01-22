044280a05b959a9e3fc81e2162b2f09bae55f19c
Nate Crossman <n8thecr8@byu.edu>
2026-01-21 22:01:09 -0700

    created custom tostring for ChessPosition.java
    Signed-off-by: Nate Crossman <n8thecr8@byu.edu>

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -48,4 +48,23 @@ public class ChessPosition {
     public int hashCode() {
         return Objects.hash(row, col);
     }
+
+    @Override
+    public String toString() {
+        char rowChar = ' '; //invalid coords will be represented by spaces
+        char colChar = ' ';
+        if (row >= 0 && row < 8) {
+            rowChar = (char) (row+1);
+        }
+        if (col == 0) {colChar = 'a';}
+        else if (col == 1) {colChar = 'b';}
+        else if (col == 2) {colChar = 'c';}
+        else if (col == 3) {colChar = 'd';}
+        else if (col == 4) {colChar = 'e';}
+        else if (col == 5) {colChar = 'f';}
+        else if (col == 6) {colChar = 'g';}
+        else if (col == 7) {colChar = 'h';}
+
+        return String.format("{%c,%c}", rowChar, colChar);
+    }
 }
