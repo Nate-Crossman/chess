04e879f3e360971e0ff44abbfa0e1c7dc0304d22
Nate Crossman <n8thecr8@byu.edu>
2026-02-26 16:38:05 -0700

    Created records for AuthData, GameData, and userData
    Signed-off-by: Nate Crossman <n8thecr8@byu.edu>

--- server/src/main/java/service/Service.java
+++ server/src/main/java/service/Service.java
@@ -1,4 +1,12 @@
 package service;
 
+import dataaccess.DataAccess;
+
 public class Service {
+
+    private final DataAccess dataAccess;
+
+    public Service(DataAccess dataAccess) {
+        this.dataAccess = dataAccess;
+    }
 }
