19375522d9b0c80d07857b9e8e2667acd0710ed7
Nate Crossman <n8thecr8@byu.edu>
2026-09-19 14:42:42 -0600

    Added function framework for ChessPiece moves.

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,6 +1,7 @@
 package chess;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Objects;
 
 /**
@@ -53,7 +54,57 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        switch (type) {
+            case KING -> {
+                return getKingMoves(board, myPosition);
+            }
+            case KNIGHT -> {
+                return getKnightMoves(board, myPosition);
+            }
+            case ROOK -> {
+                return getRookMoves(board, myPosition);
+            }
+            case BISHOP -> {
+                return getBishopMoves(board, myPosition);
+            }
+            case QUEEN -> {
+                return getQueenMoves(board, myPosition);
+            }
+            case PAWN -> {
+                return getPawnMoves(board, myPosition);
+            }
+            case null, default -> {return null;}
+        }
+    }
+
+    private Collection<ChessMove> getKingMoves(ChessBoard board, ChessPosition myPosition) {
+        Collection<ChessMove> output = new HashSet<>();
+        return output;
+    }
+
+    private Collection<ChessMove> getKnightMoves(ChessBoard board, ChessPosition myPosition) {
+        Collection<ChessMove> output = new HashSet<>();
+        return output;
+    }
+
+    private Collection<ChessMove> getRookMoves(ChessBoard board, ChessPosition myPosition) {
+        Collection<ChessMove> output = new HashSet<>();
+        return output;
+    }
+
+    private Collection<ChessMove> getBishopMoves(ChessBoard board, ChessPosition myPosition) {
+        Collection<ChessMove> output = new HashSet<>();
+        return output;
+    }
+
+    private Collection<ChessMove> getQueenMoves(ChessBoard board, ChessPosition myPosition) {
+        Collection<ChessMove> output = new HashSet<>();
+        return output;
+    }
+
+    private Collection<ChessMove> getPawnMoves(ChessBoard board, ChessPosition myPosition) {
+        Collection<ChessMove> output = new HashSet<>();
+        return output;
     }
 
     @Override
