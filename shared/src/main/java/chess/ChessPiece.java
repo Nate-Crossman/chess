4d493515d771ff62a4bc1c475771028ee1f1f095
Nate Crossman <n8thecr8@byu.edu>
2026-09-19 16:28:46 -0600

    Added Bishop Movement functionality

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -286,6 +286,10 @@ public class ChessPiece {
 
     private Collection<ChessMove> getBishopMoves(ChessBoard board, ChessPosition myPosition) {
         Collection<ChessMove> output = new HashSet<>();
+        recMoveNorthWest(board, myPosition, myPosition, output);
+        recMoveSouthWest(board, myPosition, myPosition, output);
+        recMoveSouthEast(board, myPosition, myPosition, output);
+        recMoveNorthEast(board, myPosition, myPosition, output);
         return output;
     }
 
